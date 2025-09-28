cca5f4ea6184a690466e031c04ede974c841d6c4
Jordan Hutahaean <jordanhutahaean87@gmail.com>
2025-09-28 21:26:00 +0700

    Add files via upload

---
+++ Modul1/23-071_Jordan_Hutahaean/Tugas-18.php
@@ -0,0 +1,13 @@
+<?php
+$nama = "Jordan Hutahaean";
+$nim  = "123456789";
+$kelas = "IF-10";
+$alamat = "Medan";
+
+echo "<table border='1' cellpadding='5'>
+<tr><th>Nama</th><td>$nama</td></tr>
+<tr><th>NIM</th><td>$nim</td></tr>
+<tr><th>Kelas</th><td>$kelas</td></tr>
+<tr><th>Alamat</th><td>$alamat</td></tr>
+</table>";
+?>
