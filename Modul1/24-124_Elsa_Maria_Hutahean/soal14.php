2219a8d548036b754d10fc63bbe60e87bd84305a
elsahutahean <elsahutahaean692@gmail.com>
2025-09-30 21:32:54 +0700

    Add files via upload

---
+++ Modul1/24-124_Elsa_Maria_Hutahean/soal14.php
@@ -0,0 +1,10 @@
+<?php
+function familyName($fname){
+    echo "$fname Refsnes.<br>";
+}
+familyName("Janji");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
+?>
