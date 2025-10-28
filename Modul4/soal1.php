f2f9d3aae64814d489e8bc70321e1ac17166c1a4
Ifandi-wahyu <wifandi6@gmail.com>
2025-10-28 23:13:52 +0700

    Add files via upload

---
+++ Modul4/soal1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form Input Surname</title>
+</head>
+<body>
+    <h2>Form Input Surname</h2>
+
+    <form method="post" action="soal1.php">
+        <label>Masukkan Surname:</label><br>
+        <input type="text" name="surname" required>
+        <br><br>
+        <input type="submit" value="Kirim">
+    </form>
+</body>
+</html>
