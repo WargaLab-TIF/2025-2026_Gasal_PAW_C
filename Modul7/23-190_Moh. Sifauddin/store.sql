57f5a019bf540b6238797a5fbc533ef7723f29bd
sifa-190 <161006527+sifa-190@users.noreply.github.com>
2025-11-19 10:31:43 +0700

    Add files via upload

---
+++ Modul7/23-190_Moh. Sifauddin/store.sql
@@ -0,0 +1,335 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 19 Nov 2025 pada 03.30
+-- Versi server: 8.0.30
+-- Versi PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int NOT NULL,
+  `kode_barang` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_barang` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `harga` int NOT NULL,
+  `stok` int NOT NULL,
+  `supplier_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `harga`, `stok`, `supplier_id`) VALUES
+(1, '1', 'buku', 5000, 25, 1),
+(2, '2', 'pensil', 3000, 20, 2),
+(3, '3', 'pulpen', 4000, 55, 3),
+(4, '4', 'penggaris', 4000, 40, 4),
+(5, '5', 'krayon', 18000, 35, 5),
+(6, '6', 'jangka', 7500, 100, 6),
+(7, '7', 'stabilo', 3500, 120, 7),
+(8, '8', 'spidol', 11000, 60, 8),
+(9, '9', 'kapur', 2000, 130, 9),
+(10, '10', 'map', 2000, 130, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id` int NOT NULL,
+  `nama` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `jenis_kelamin` enum('L','P') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `telp` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id`, `nama`, `jenis_kelamin`, `telp`, `alamat`) VALUES
+(1, 'Leha', 'P', '085805282300', 'Gresik'),
+(2, 'Yogi', 'L', '085655543781', 'Banyuwangi'),
+(3, 'Tegar', 'L', '085766630242', 'Yogyakarta'),
+(4, 'Raehan', 'L', '081515686224', 'Tulungagung'),
+(5, 'Della', 'P', '083837253237', 'Bali'),
+(6, 'Shilvy', 'P', '085738692081', 'Surabaya'),
+(7, 'Sari', 'P', '085604183247', 'Lamongan'),
+(8, 'Sifa', 'L', '085706357310', 'Lamongan'),
+(9, 'Dudit', 'L', '089648740209', 'Lamongan'),
+(10, 'Ahmad', 'L', '087860057238', 'Madura');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id` int NOT NULL,
+  `waktu_bayar` datetime NOT NULL,
+  `total` int NOT NULL,
+  `metode` enum('TUNAI','TRANSFER','EDC') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `transaksi_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id`, `waktu_bayar`, `total`, `metode`, `transaksi_id`) VALUES
+(1, '2024-10-18 07:49:33', 85000, 'TUNAI', 1),
+(2, '2024-10-19 09:30:58', 110000, 'TUNAI', 2),
+(3, '2024-10-19 19:51:36', 50000, 'TUNAI', 3),
+(4, '2024-10-20 14:12:19', 94000, 'TRANSFER', 4),
+(5, '2024-10-21 16:43:30', 165000, 'EDC', 5),
+(6, '2024-10-22 08:55:31', 45000, 'TRANSFER', 6),
+(7, '2024-10-23 20:06:10', 56000, 'TUNAI', 7),
+(8, '2024-10-24 07:56:57', 105000, 'TRANSFER', 8),
+(9, '2024-10-25 09:51:27', 76000, 'TRANSFER', 9),
+(10, '2024-10-25 11:38:05', 145000, 'EDC', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int NOT NULL,
+  `nama` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `telp` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `nama`, `telp`, `alamat`) VALUES
+(1, 'PT. Maju Bersama', '0123456', 'Surabaya'),
+(2, 'PT. Senang Sekali', '081515563', 'Bangkalan'),
+(3, 'PT. Pulpen Makmur', '08122334455', 'Lamongan'),
+(4, 'PT. Penggaris Jaya', '08199887766', 'Gresik'),
+(5, 'PT.Segar Segar', '081703316523', 'surabaya barat '),
+(6, 'PT. Alat Ukur Hebat', '081234556677', 'Sidoarjo'),
+(7, 'PT. Stabilo Ceria', '089533221100', 'Madura'),
+(8, 'PT. Spidol Abadi', '08561237890', 'Surabaya'),
+(9, 'PT. Kapur Putih', '08579992211', 'Sampang'),
+(10, 'PT. Map Mapan', '08523144566', 'Pamekasan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int NOT NULL,
+  `waktu_transaksi` date NOT NULL,
+  `keterangan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `total` int NOT NULL DEFAULT '0',
+  `pelanggan_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `waktu_transaksi`, `keterangan`, `total`, `pelanggan_id`) VALUES
+(1, '2025-11-08', 'Kebutuhan Kelas', 85000, 1),
+(2, '2025-11-09', 'kebutuhan Pribadi', 110000, 2),
+(3, '2025-11-10', 'Kebutuhan Kelas', 50000, 3),
+(4, '2025-11-11', 'kebutuhan Pribadi', 94000, 4),
+(5, '2025-11-12', 'Kebutuhan kelas', 72000, 5),
+(6, '2025-11-11', 'Kebutuhan Kelas', 45000, 6),
+(7, '2025-11-13', 'Kebutuhan Kelas', 56000, 7),
+(8, '2025-11-14', 'kebutuhan Kelas', 105000, 8),
+(9, '2025-11-14', 'Kebutuhan Pribadi', 5000, 9),
+(10, '2025-11-17', 'Pembelian Alat Tulis', 65000, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `transaksi_id` int NOT NULL,
+  `barang_id` int NOT NULL,
+  `harga` int NOT NULL,
+  `qty` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `transaksi_detail`
+--
+
+INSERT INTO `transaksi_detail` (`transaksi_id`, `barang_id`, `harga`, `qty`) VALUES
+(9, 1, 5000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` tinyint NOT NULL,
+  `username` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nama` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `hp` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `level` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`supplier_id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `transaksi_id` (`transaksi_id`);
+
+--
+-- Indeks untuk tabel `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `pelanggan_id` (`pelanggan_id`);
+
+--
+-- Indeks untuk tabel `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD PRIMARY KEY (`transaksi_id`,`barang_id`),
+  ADD KEY `transaksi_id` (`transaksi_id`),
+  ADD KEY `barang_id` (`barang_id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  MODIFY `transaksi_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` tinyint NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`id`) REFERENCES `transaksi` (`pelanggan_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `supplier`
+--
+ALTER TABLE `supplier`
+  ADD CONSTRAINT `supplier_ibfk_1` FOREIGN KEY (`id`) REFERENCES `barang` (`supplier_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD CONSTRAINT `transaksi_detail_ibfk_1` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_detail_ibfk_2` FOREIGN KEY (`transaksi_id`) REFERENCES `transaksi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
